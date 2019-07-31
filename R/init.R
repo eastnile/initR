d532aa9326b13a9949efef3893134d0092a548ce
Zhaochen He <sterilite@Zhaochens-MacBook-Air.local>
2019-07-31 12:35:43 +0800

    v3

---
+++ R/init.R
@@ -0,0 +1,23 @@
+# get gdrivepath
+gdrivepath = function() {
+  path = as.character(gdrivepaths[gdrivepaths$sysname == Sys.info()["nodename"],2])
+  return(path)
+}
+
+# install if not present
+installif = function(libs) {
+  new.packages = libs[!(libs %in% installed.packages()[,'Package'])]
+  if(length(new.packages)) {install.packages(new.packages)}
+}
+
+# Load a string array of packages
+lib = function(libs) {
+  lapply(libs, library, character.only = TRUE)
+}
+
+# combine installif and lib
+libif = function(libs) {
+  installif(libs)
+  lib(libs)
+}
+
